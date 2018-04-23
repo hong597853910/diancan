1ef3a74e4e4569bf8a09680b650d099c125032a3
59785 <wangxihong@utry.cn>
2018-04-23 22:07:42 +0800

    增大添加购物车按钮

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -3,15 +3,15 @@
   <div class="cartcontrol">
     <transition name="fadeRotate">
       <div class="cart-decrease" v-show="food.count>0" @click.stop.prevent="decreaseCart()">
-        <i class="icon iconfont icon-offline_fill"></i>
-          <span class="icon-remove_circle_outline inner"></span>
+        <i class="icon iconfont icon-offline_fill opr-btn"></i>
+          <!-- <span class="icon-remove_circle_outline inner"></span> -->
       </div>
     </transition>
     <div class="cart-count" v-show="food.count>0">
       {{food.count}}
     </div>
     <div class="cart-add" @click.stop.prevent="addCart()">
-      <i class="icon iconfont icon-addition"></i>
+      <i class="icon iconfont icon-addition opr-btn"></i>
     </div>
   </div>
 
@@ -54,6 +54,7 @@ export default {
     color rgb(0,160,220)
     display inline-block
     padding 6px
+    line-height 24px
     transition: all .4s linear
     .inner
       line-height 24px
@@ -80,9 +81,10 @@ export default {
     padding 6px 0
   .cart-add
     display inline-block
-    vertical-align top
     font-size 24px
     color rgb(0,160,220)
     line-height 24px
     padding 6px
+  .opr-btn
+    font-size 20px
 </style>
